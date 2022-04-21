86c5ff720c42d8d9be7191de6912d07808800749
lkord <lkordowski@gmail.com>
2022-04-21 22:09:36 +0200

    ssh section

---
+++ data/L10/aws-script.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+DATE=$(date +%H-%M-%S)
+BACKUP=db-$DATE.sql
+
+DB_HOST=$1
+DB_PASSWORD=$2
+DB_NAME=$3
+AWS_SECRET=$4
+BUCKET_NAME=$5
+
+mysqldump -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /tmp/$BACKUP
+
+export AWS_ACCESS_KEY_ID=AKIAWOJ5MEVUMDBUBLPN && \
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
+echo "Uploading db backup" && \
+aws s3 cp /tmp/$BACKUP s3://$BUCKET_NAME/$BAKCUP
